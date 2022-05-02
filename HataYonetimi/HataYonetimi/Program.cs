20598a9c02935e65f9997445fb6b69bb06a351fb
Nilufer Ulgen <90576291+NULGEN@users.noreply.github.com>
2022-05-03 00:38:51 +0300

    Hata Yonetimi dosyası eklendi

---
+++ HataYonetimi/HataYonetimi/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HataYonetimi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Bir sayı giriniz: ");
+                int sayi = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Girmiş olduğunuz sayı: " + sayi);
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Hatalı giriş yapıldı"+ ex.Message.ToString());
+            }
+            finally // opsiyonel. Her şartta bu blok çalışır.
+            {
+                Console.WriteLine("İşlem Tamamlandı");
+            }
+
+            try
+            {
+               // int a = int.Parse(null); // ArgumentNull Exception
+               // int a= int.Parse("test"); // Format Exception
+                int b = int.Parse("-200000000000"); // Overflow Exception
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Boş değer girdiniz.");
+                Console.WriteLine(ex);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Veri tipi uygun değil");
+                Console.WriteLine(ex);            
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("Çok küçük yada çok büyük bir değer girdiniz");
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.WriteLine("İşlem başarı ile tamamlandı");
+            }
+            
+            
+            
+            
+           
+        }
+    }
+}
